8aa893e41ee6e1749d54f64ea152fd5e4b18105e
PenToPaper <wheelm11@gmail.com>
2019-08-14 18:42:36 -0400

    Added simple javascript article switching

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+//onload
+document.addEventListener("DOMContentLoaded", function(event) {
+    collectionBind(document.getElementsByClassName("timeline-node"), "click", onTimelineNodeClick);
+});
+
+function collectionBind(collection, listenerType, func) {
+    for (var i = 0; i < collection.length; i++) {
+        collection[i].addEventListener(listenerType, function(event) {
+            return func(event, this);
+        });
+    }
+}
+
+function onTimelineNodeClick(event, element) {
+    var articleName = element.getAttribute("data-article");
+
+    selectArticle(articleName + "-article");
+    selectSidebarNode(articleName + "-node");
+}
+
+function selectArticle(articleName) {
+    var articles = document.getElementsByTagName("article");
+    for (var i = 0; i < articles.length; i++) {
+        if (articles[i].id == articleName) {
+            articles[i].className = "";
+        } else {
+            articles[i].className = "article-hidden";
+        }
+    }
+}
+
+function selectSidebarNode(articleName) {
+    var sidebarNodes = document.getElementsByClassName("timeline-node");
+    for (var i = 0; i < sidebarNodes.length; i++) {
+        sidebarNodes[i].className = sidebarNodes[i].className.replace(/\s?timeline-node-selected\s?/, "");
+        if (sidebarNodes[i].id == articleName) {
+            sidebarNodes[i].className = sidebarNodes[i].className + " timeline-node-selected";
+        }
+    }
+}

965de6438f1454bfc3b1b9556672721aa96020f9
PenToPaper <wheelm11@gmail.com>
2019-08-24 19:55:01 -0400

    Refined animation style in transition

--- scripts/index.js
+++ scripts/index.js
@@ -18,8 +18,8 @@ function AtomAnimation(elementId, jsonLocation) {
     // Static
 
     // Each key represents a scientist
-    // Array[0] = first frame of animation with that scientist's work
-    // Array[1] = last frame of animation with that scientist's work
+    // Array[0] = first frame where that scientist's work is being animated in
+    // Array[1] = first frame where the scientist's work animation is complete
     this.scientistFrames = {
         democritus: [0, 20],
         dalton: [40, 60],
@@ -28,6 +28,7 @@ function AtomAnimation(elementId, jsonLocation) {
         bohr: [200, 260],
         schrodinger: [280, 319]
     };
+    this.scientistOrder = ["democritus", "dalton", "thomson", "rutherford", "bohr", "schrodinger"];
 
     // Assigned on construction, not changed afterwards
     this.elementId = elementId;
@@ -41,25 +42,96 @@ function AtomAnimation(elementId, jsonLocation) {
     });
 
     // Dynamic
-    // TODO Implement this.playing for debugging
+    // If animation is being played, the currently displayed model. If none being played, false
     this.playing = false;
+    // The last scientist that an animation was played from. Used to calculate current frame with Lottie/Bodymovin
+    this.lastScientistFrom = "democritus";
 
-    this.playFrom = function(scientistFrom, scientistTo, callback) {
+    // Plays from one scientist in the list to another scientist in the list. Works forwards and backwards.
+    // callback called on completion of the animation segment
+    // transitionScientistCallback called when during an animation a model of a different scientist is animated to. Gets passed the new scientist's name
+    this.playFrom = function(scientistFrom, scientistTo, callback, transitionScientistCallback) {
+        // Makes sure the scientists passed are valid
         if (!(scientistFrom in this.scientistFrames) || !(scientistTo in this.scientistFrames)) {
             return false;
         }
 
-        if (this.scientistFrames[scientistFrom][0] <= this.scientistFrames[scientistTo][0]) {
-            // Moving forward in time
+        // Plays the segments
+        if (scientistFrom == scientistTo) {
             this.animation.playSegments([this.scientistFrames[scientistFrom][0], this.scientistFrames[scientistTo][1]], true);
         } else {
-            // Moving backwards in time
             this.animation.playSegments([this.scientistFrames[scientistFrom][1], this.scientistFrames[scientistTo][1]], true);
         }
 
-        this.animation.removeEventListener("complete", callback);
-        this.animation.addEventListener("complete", callback);
+        // Helper local functions for callbacks
+        var onComplete = function() {
+            this.playing = false;
+            callback();
+        }.bind(this);
+
+        var onEnterFrame = function(event) {
+            this.handleEnterFrame(event, transitionScientistCallback);
+        }.bind(this);
+
+        this.playing = scientistFrom;
+        this.lastScientistFrom = scientistFrom;
+        this.animation.removeEventListener("enterFrame", onEnterFrame);
+        this.animation.removeEventListener("complete", onComplete);
+        this.animation.addEventListener("complete", onComplete);
+        if (transitionScientistCallback) {
+            this.animation.addEventListener("enterFrame", onEnterFrame);
+            transitionScientistCallback(this.playing);
+        }
     };
+
+    this.handleEnterFrame = function(event, callback) {
+        if (event.direction == 1) {
+            // Moving forward
+            var currentScientistIndex = this.scientistOrder.indexOf(this.playing);
+            // Checks if we've exceeded the # of scientists in the list
+            if (currentScientistIndex < 0 || currentScientistIndex + 1 >= this.scientistOrder.length) {
+                return false;
+            }
+            var nextScientistIndex = currentScientistIndex + 1;
+
+            /**
+             * Based on the model:
+             *
+             * 0   20   40   60   80   100   110
+             * Democr   DaltonD   ThomsonThomson
+             * -------->
+             *
+             * If moving from this direction, we want this if statement to be true when the current frame passes the next scientist's first frame
+             */
+            if (this.scientistFrames[this.scientistOrder[nextScientistIndex]][0] <= this.scientistFrames[this.lastScientistFrom][1] + event.currentTime) {
+                this.playing = this.scientistOrder[nextScientistIndex];
+                callback(this.playing);
+            }
+        } else {
+            // Moving backward
+            var currentScientistIndex = this.scientistOrder.indexOf(this.playing);
+            // Checks if we've gone below an index of 0
+            if (currentScientistIndex - 1 < 0) {
+                return false;
+            }
+            var lastScientistIndex = currentScientistIndex - 1;
+
+            /**
+             * Based on the model:
+             *
+             * 0   20   40   60   80   100   110
+             * Democr   DaltonD   ThomsonThomson
+             *          <------
+             *
+             * If moving from this direction, we want this if statement to be true when the current frame passes the current scientist's first frame
+             */
+            if (this.scientistFrames[this.playing][0] >= this.scientistFrames[this.lastScientistFrom][1] - event.totalTime + event.currentTime) {
+                this.playing = this.scientistOrder[lastScientistIndex];
+                callback(this.playing);
+            }
+        }
+    };
+    this.handleEnterFrame = this.handleEnterFrame.bind(this);
 
     // Init function, listens for when animation is loaded, then plays first scientist animation
     this.init = function(activeScientist, callback) {
@@ -98,7 +170,17 @@ function ChemTAS(activeScientist, animationElementId, animationPath) {
         this.changeActiveScientist(this.activeScientist);
         this.hideAllArticles();
         this.showAnimation();
-        this.atomAnimation.playFrom(prevActiveScientist, this.activeScientist, this.handleAnimationDone.bind(this));
+        this.atomAnimation.playFrom(prevActiveScientist, this.activeScientist, this.handleAnimationDone.bind(this), this.handleScientistTransition.bind(this));
+    };
+
+    // Handles a point in the animation where a scientist's model transitions into another scientists's model.
+    // Changes animation background color
+    // Changes animation header text
+    this.handleScientistTransition = function(newScientist) {
+        var animation = document.getElementById(this.atomAnimation.elementId + "-container");
+        animation.className = newScientist + "-animation";
+        var newScientistLabel = newScientist.charAt(0).toUpperCase() + newScientist.slice(1);
+        animation.children[0].innerHTML = newScientistLabel;
     };
 
     // Shows the animation html object
